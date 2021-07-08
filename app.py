88b26c9284df6f9ffbefc282c46b466989bc4930
avinash-vk <avinash2000vk@gmail.com>
2021-07-08 17:28:08 +0530

    added ui for testing

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from routers import v1
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates/")
 
 @app.get("/")
-def index():
-    return { "message" : "Server is running successfully"}
+def index(request:Request):
+    return templates.TemplateResponse('test.html', context={'request': request})
 
 @app.get('/ping')
 def ping():
     return {'message': 'pong'}
 
+
 app.include_router(
     v1.router,
     prefix="/api/v1"
-);
+)
 
 if __name__ == '__main__':
     uvicorn.run(app, host="127.0.0.1", port=8000)
