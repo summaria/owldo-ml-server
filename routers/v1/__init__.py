48df5c280ce18fbc5aee5da9d3c0bcb76fa55a8d
avinash-vk <avinash2000vk@gmail.com>
2021-09-02 17:52:11 +0530

    added tmp html

--- routers/v1/__init__.py
+++ routers/v1/__init__.py
@@ -4,11 +4,11 @@ from .ts_router import router as ts_router
 
 router = APIRouter()
 
-# router.include_router(
-#     qg_router,
-#     prefix='/qg',
-#     tags=['question-generation'],
-# );
+router.include_router(
+    qg_router,
+    prefix='/qg',
+    tags=['question-generation'],
+);
 
 router.include_router(
     ts_router,
