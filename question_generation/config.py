bebb424ceada9a282f2f8d4aeb4ae9d15c4d094b
Avinash V K <avinash2000vk@gmail.com>
2021-10-17 13:28:58 +0530

    added config changes

--- question_generation/config.py
+++ question_generation/config.py
@@ -1,43 +1,5 @@
-QG_MODEL = "summaria/qa-qg-t5"
-ANS_MODEL = "summaria/qa-t5"
-MCQ_MODEL = "D:\\programs\\projects\\side\\summaria\\ml-server\\models\\t5-race-qa-2\\"
-QA_QG_MODEL = "D:\\programs\\projects\\side\\summaria\\ml-server\\models\\t5-large-best-model\\"
-
-QG_MODEL_ARGS = {
-        "reprocess_input_data": True,
-        "overwrite_output_dir": True,
-        "max_seq_length": 128,
-        "eval_batch_size": 128,
-        "num_train_epochs": 1,
-        "save_eval_checkpoints": False,
-        "use_multiprocessing": False,
-        "num_beams": None,
-        "do_sample": True,      
-        "max_length": 50,
-        "top_k": 50,
-        "top_p": 0.95,
-        "num_return_sequences": 3,
-        "fp16":False,
-        "silent":True
-}
-
-ANS_MODEL_ARGS = {
-        "reprocess_input_data": True,
-        "overwrite_output_dir": True,
-        "max_seq_length": 128,
-        "eval_batch_size": 128,
-        "num_train_epochs": 1,
-        "save_eval_checkpoints": False,
-        "use_multiprocessing": False,
-        "num_beams": None,
-        "do_sample": True,
-        "max_length": 50,
-        "top_k": 50,
-        "top_p": 0.75,
-        "num_return_sequences": 1,
-        "fp16":False,
-        "silent":True
-}
+MCQ_MODEL = os.environ.get("MCQ_MODEL") or "D:\\programs\\projects\\side\\summaria\\ml-server\\models\\t5-race-qa-2\\"
+QA_QG_MODEL = os.environ.get("QA_QG_MODEL") or "D:\\programs\\projects\\side\\summaria\\ml-server\\models\\t5-large-best-model\\"
 
 MCQ_MODEL_ARGS = {
     "reprocess_input_data": True,
@@ -53,7 +15,7 @@ MCQ_MODEL_ARGS = {
     "top_k": 50,
     "top_p": 0.95,
     "num_return_sequences": 3,
-    "fp16":False,
+    "fp16": os.environ.get("USE_CUDA") or False,
     "silent":True
 }
 
--- summarization/config.py
+++ summarization/config.py
@@ -1,4 +1,4 @@
-S_MODEL = "summaria/pegasus"
+S_MODEL = os.environ.get("SUMMARIZATION_MODEL") or "summaria/pegasus"
 EASY=0.6
 MEDIUM=0.4
 EXTENSIVE=0.3
